95a3d65e2e078b9df32c043544efb810fe5cae73
Glitch (dew-pet) <none>
2019-02-05 18:44:22 +0000

    🏎😺 Checkpoint ./routes/api.js:2619925/899

--- routes/api.js
+++ routes/api.js
@@ -20,9 +20,32 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
+      search('dev1')
+      res.send('Work in Progress')
     });
     
 };
 
-let search(key) 
+let search = key => {
+  key = key.toUpperCase()
+  db.collection('stock').findOne({'stock': key}, (err, doc) => {
+    if (doc == null) {
+      
+      db.collection('stock').insertOne({'stock': key}, (err, doc) => {
+        return doc.ops[0]
+      })
+    }
+  })
+  
+}
+
+function loadDoc(key) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            
+       }
+    };
+    xhttp.open("GET", api+key, true);
+    xhttp.send();
+}
