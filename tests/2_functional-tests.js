aa100ca2d785c693357f21a14dcff8a4d97cc62d
Glitch (dew-pet) <none>
2019-02-06 18:32:14 +0000

    🍽💒 Checkpoint ./tests/2_functional-tests.js:2619925/120

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -13,7 +13,7 @@ var server = require('../server');
 
 chai.use(chaiHttp);
 
-let test = {stock: 'goog'}
+let testStock = {stock: 'goog'}
 
 suite('Functional Tests', function() {
     
@@ -22,14 +22,14 @@ suite('Functional Tests', function() {
       test('1 stock', function(done) {
        chai.request(server)
         .get('/api/stock-prices')
-        .query({stock: test.stock})
+        .query({stock: testStock.stock})
         .end(function(err, res){
-         
+          console.log(res)
           assert.equal(res.status, 200)
-          assert.equal(res.body.stockData.stock, test.stock)
+          assert.equal(res.body.stockData.stock, testStock.stock)
           
-          test.price = res.body.stockData.price
-          test.like = res.body.stockData.like
+          testStock.price = res.body.stockData.price
+          testStock.like = res.body.stockData.like
          
           done();
         });
@@ -38,13 +38,13 @@ suite('Functional Tests', function() {
       test('1 stock with like', function(done) {
         chai.request(server)
         .get('/api/stock-prices')
-        .query({stock: test.stock})
+        .query({stock: testStock.stock})
         .end(function(err, res){
          
           assert.equal(res.status, 200)
-          assert.equal(res.body.stockData.stock, test.stock)
-          assert.equal(res.body.stockData.like, test.like+1)
-          test.like = res.body.stockData.like
+          assert.equal(res.body.stockData.stock, testStock.stock)
+          assert.equal(res.body.stockData.like, testStock.like+1)
+          testStock.like = res.body.stockData.like
          
           done();
         });
@@ -53,13 +53,13 @@ suite('Functional Tests', function() {
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
         chai.request(server)
         .get('/api/stock-prices')
-        .query({stock: test.stock})
+        .query({stock: testStock.stock})
         .end(function(err, res){
          
           assert.equal(res.status, 200)
-          assert.equal(res.body.stockData.stock, test.stock)
-          assert.equal(res.body.stockData.like, test.like)
-          test.like = res.body.stockData.like
+          assert.equal(res.body.stockData.stock, testStock.stock)
+          assert.equal(res.body.stockData.like, testStock.like)
+          testStock.like = res.body.stockData.like
          
           done();
         });
@@ -68,7 +68,7 @@ suite('Functional Tests', function() {
       test('2 stocks', function(done) {
         chai.request(server)
         .get('/api/stock-prices')
-        .query({stock: [test.stock, 'msft']})
+        .query({stock: [testStock.stock, 'msft']})
         .end(function(err, res){
          
           assert.equal(res.status, 200)
@@ -83,7 +83,7 @@ suite('Functional Tests', function() {
       test('2 stocks with like', function(done) {
         chai.request(server)
         .get('/api/stock-prices')
-        .query({stock: [test.stock, 'msft'], like:true})
+        .query({stock: [testStock.stock, 'msft'], like:true})
         .end(function(err, res){
          
           assert.equal(res.status, 200)
