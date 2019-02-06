837e943502ed17ac6348cbd82b2e35ad9bf647a5
Glitch (dew-pet) <none>
2019-02-06 13:36:19 +0000

    🚆❤️ Checkpoint ./routes/api.js:2619925/2478

--- routes/api.js
+++ routes/api.js
@@ -26,7 +26,36 @@ module.exports = function (app) {
     let stockData = {}
       
       if (Array.isArray(req.query.stock)) {
-        
+        let stockArray = []
+        req.query.stock.forEach(stock => {
+          request({
+              url: api+stock,
+              json: true
+          }, function (error, response, body) {
+              if (!error && response.statusCode === 200) {
+                  let stock = { stock: stock, price: body['Global Quote']['05. price'], like: 1}
+                  let update
+                  if ( req.query.hasOwnProperty('like') && req.query.like ) {
+                    update = {$set: {price: stock.price}, $inc: {like: +1}}
+                  } else {
+                    update = {$set: {price: stock.price}, $setOnInsert: {like: 0}}
+                  }
+                  db.collection('stock').findOneAndUpdate(
+                      {'stock': stock.stock},
+                      update,
+                      {upsert: true, returnOriginal: false},
+                      (err, doc) => {
+                        if (err) {
+                          console.log(err)
+                          res.send('error')
+                        } else {
+                          stockArray.push({stock: doc.value.stock, price: doc.value.price, rel})
+                        }
+                      }
+                    )
+              }
+          })
+        })
       } else {
           request({
             url: api+req.query.stock,
@@ -37,8 +66,8 @@ module.exports = function (app) {
                 let update
                 if ( req.query.hasOwnProperty('like') && req.query.like ) {
                   update = {$set: {price: stock.price}, $inc: {like: +1}}
-                } else if () {
-                  update = {$set: {price: stock.price}, $setOnInsert: {like: }}
+                } else {
+                  update = {$set: {price: stock.price}, $setOnInsert: {like: 0}}
                 }
                 db.collection('stock').findOneAndUpdate(
                     {'stock': stock.stock},
@@ -49,7 +78,7 @@ module.exports = function (app) {
                         console.log(err)
                         res.send('error')
                       } else {
-                        res.json({stockData: doc.value})
+                        res.json({stockData: {stock: doc.value.stock, price: doc.value.price, like: doc.value.like}})
                       }
                     }
                   )
